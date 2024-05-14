ad069650e2e5dcaedc8bcfa03e51153f2ce2d88b
Jeziel Monteiro <luiz.m.jeziel@gmail.com>
2024-05-14 19:29:30 -0300

    Add congresso

---
+++ Exercícios/congresso/palestrante.py
@@ -0,0 +1,12 @@
+class Palestrante:
+    def __init__(self, id, nome, img, biografia, area, linkedin, github, instagram, sessao, avaliacao) -> None:
+        self.id = id
+        self.nome = nome
+        self.img = img
+        self.biografia = biografia
+        self.area = area
+        self.linkedin = linkedin
+        self.github = github
+        self.instagram = instagram
+        self.sessao = sessao
+        self.avaliacao = avaliacao
---
+++ Exercícios/congresso/sessao.py
@@ -0,0 +1,9 @@
+class Sessao:
+    def __init__(self, id, nome, img, descricao, horarios, localizacao, comentarios) -> None:
+        self.id = id
+        self.nome = nome
+        self.img = img
+        self.descricao = descricao
+        self.horarios = horarios
+        self.localizacao = localizacao
+        self.comentarios = comentarios
