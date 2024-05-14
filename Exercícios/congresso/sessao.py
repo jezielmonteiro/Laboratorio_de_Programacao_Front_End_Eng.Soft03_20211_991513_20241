bd47c41ef535e0f94f7a1da3c29b468322e47f65
Jeziel Monteiro <luiz.m.jeziel@gmail.com>
2024-05-14 19:29:02 -0300

    Delete congresso

--- Exercícios/congresso/sessao.py
+++
@@ -1,9 +0,0 @@
-class Sessao:
-    def __init__(self, id, nome, img, descricao, horarios, localizacao, comentarios) -> None:
-        self.id = id
-        self.nome = nome
-        self.img = img
-        self.descricao = descricao
-        self.horarios = horarios
-        self.localizacao = localizacao
-        self.comentarios = comentarios
